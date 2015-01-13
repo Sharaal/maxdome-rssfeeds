68fc470169bcbd4cc95dab83883e181f8b89d436
Christoph Herrmann <developer@dragonprojects.de>
2015-01-13 22:43:28 +0100

    Update documentation to the current config structure

--- modules/rss.js
+++ modules/rss.js
@@ -8,8 +8,7 @@
             {
                 route: '',
                 channel: {
-                    title: '',
-                    description: '',
+                    name: '',
                     link: ''
                 },
                 parser: {
