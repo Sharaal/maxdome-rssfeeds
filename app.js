9442e70d60d263dea6b67028127b7218285892ef
Christoph Herrmann <developer@dragonprojects.de>
2015-01-14 21:46:13 +0100

    Debug cause of connection error to redis server

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var config = {
         npm: {
             'dragonnodejs-redis': {
                 client: (function () {
-                    console.log(process.env.REDISCLOUD_URL);
                     if (process.env.REDISCLOUD_URL) {
                         return { uri: process.env.REDISCLOUD_URL };
                     }
