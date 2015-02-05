25918b150838e1dad345f8f3396b7116e15ce16c
Christoph Herrmann <developer@dragonprojects.de>
2015-02-05 22:36:56 +0100

    Add new relic Node.js agent

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// Add new relic Node.js agent
+
+if (process.env.NEW_RELIC_APP_NAME
+    && process.env.NEW_RELIC_LICENSE_KEY
+    && process.env.NEW_RELIC_LEVEL) {
+    require('newrelic');
+}
+
 // Load the libraries and modules
 
 var config = {
