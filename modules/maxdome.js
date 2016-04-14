c0088a928b2711ab7e77ba15cf5498a38b2f184b
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-14 10:33:18 +0200

    Remove currently the descriptions

--- modules/maxdome.js
+++ modules/maxdome.js
@@ -13,7 +13,6 @@ module.exports = function (config, libraries, services) {
 
     var maxdome = function (parser, callback) {
         crawler(parser.crawlerurl, function (err, $) {
-            var parallels = [];
             var items = [];
             $('ul.module-s4--covers div[data-react-props]').each(function(i, div) {
                 div = $(div);
@@ -31,25 +30,8 @@ module.exports = function (config, libraries, services) {
                     }
                 };
                 items.push(item);
-                parallels.push(function (callback) {
-                    cache(
-                        'item:' + item.guid,
-                        function (callback) {
-                            crawler(item.link, function (err, $) {
-                                var value = { description: $('meta[property="og:description"]').attr('content') };
-                                callback(value);
-                            });
-                        },
-                        function (value) {
-                            item.description = value.description;
-                            callback();
-                        }
-                    );
-                });
             });
-            async.parallel(parallels, function () {
-                callback(items);
-            });
+            callback(items);
         });
     };
 
