cb0004e8ed72e0ad65228dd12734b4705258f6d0
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-26 16:06:48 +0200

    Simplify configuration

--- src/configs.js
+++ src/configs.js
@@ -1,44 +1,20 @@
-const channels = {
-  maxdome: (config) => {
+const configs = {
+  maxdome: (title, config) => {
     return {
-      link: { package: 'http://www.maxdome.de', store: 'http://store.maxdome.de'}[config.area],
-      title: config.title,
-      ttl: 10
-    };
-  }
-};
-
-const proxies = {
-  maxdome: (config) => {
-    return {
-      name: 'maxdome',
-      config: Object.assign(
-        { apikey: 'f6cb16de3b67b8b9d64d047a00a9dc55', appid: '4a5fa26b', clienttype: 'Webportal' },
-        config
-      )
-    };
+      channel: {
+        link: { package: 'http://www.maxdome.de', store: 'http://store.maxdome.de'}[config.area],
+        title,
+        ttl: 10
+      },
+      route: '/' + config.area + '-' + config.type,
+      proxy: { name: 'maxdome', config }
+    }
   }
 };
 
 export default [
-  {
-    channel: channels.maxdome({ area: 'package', title: 'Neue Serien im Paket' }),
-    route: '/package-series',
-    proxy: proxies.maxdome({ area: 'package', type: 'series' })
-  },
-  {
-    channel: channels.maxdome({ area: 'package', title: 'Neue Filme im Paket' }),
-    route: '/package-movies',
-    proxy: proxies.maxdome({ area: 'package', type: 'movies' })
-  },
-  {
-    channel: channels.maxdome({ area: 'store', title: 'Neue Serien im Store' }),
-    route: '/store-series',
-    proxy: proxies.maxdome({ area: 'store', type: 'series' })
-  },
-  {
-    channel: channels.maxdome({ area: 'store', title: 'Neue Filme im Store' }),
-    route: '/store-movies',
-    proxy: proxies.maxdome({ area: 'store', type: 'movies' })
-  }
+  configs.maxdome('Neue Serien im Paket', { area: 'package', type: 'series' }),
+  configs.maxdome('Neue Filme im Paket', { area: 'package', type: 'movies' }),
+  configs.maxdome('Neue Serien im Store', { area: 'store', type: 'series' }),
+  configs.maxdome('Neue Filme im Store', { area: 'store', type: 'movies' })
 ];
--- src/proxies/maxdome.js
+++ src/proxies/maxdome.js
@@ -3,20 +3,25 @@ import request from 'request';
 export default (config) => async () => {
   return new Promise((resolve) => {
     const options = {
-      url: 'https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?filter[]=%type%&filter[]=new&filter[]=notUnlisted&sort[]=activeLicenseStart~desc&filter[]=%area%&appid=%appid%&apikey=%apikey%'
-        .replace('%appid%', config.appid)
-        .replace('%apikey%', config.apikey)
-        .replace('%area%', { package: 'hasPackageContent', store: 'availableWithoutPackage'}[config.area])
-        .replace('%type%', { movies: 'movies', series: 'seasons' }[config.type]),
-      headers: { clienttype: config.clienttype, 'Maxdome-Origin': 'de', Accept: 'application/json' }
+      url: 'https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?' +
+        [
+          'apikey=f6cb16de3b67b8b9d64d047a00a9dc55',
+          'appid=4a5fa26b',
+          'filter[]=new',
+          'filter[]=notUnlisted',
+          'filter[]=' + { package: 'hasPackageContent', store: 'availableWithoutPackage'}[config.area],
+          'filter[]=' + { movies: 'movies', series: 'seasons' }[config.type],
+          'sort[]=activeLicenseStart~desc'
+        ].join('&'),
+      headers: { accept: 'application/json', clienttype: 'Webportal', 'maxdome-origin': 'de' }
     };
     request(options, (error, response, body) => {
       const items = JSON.parse(body).assetList.map((asset) => {
         return {
+          description: asset.descriptionShort,
           guid: asset.id,
-          title: asset.title,
           link: { package: 'http://www.maxdome.de/%assetId%', store: 'http://store.maxdome.de/%assetId%'}[config.area].replace('%assetId%', asset.id),
-          description: asset.descriptionShort
+          title: asset.title
         };
       });
       resolve(items);
