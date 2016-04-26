433ccba5374f417f67c719a7f431e5cb09442846
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-26 13:34:52 +0200

    Add ttl of 10 minutes to the channel

--- src/configs.js
+++ src/configs.js
@@ -1,16 +1,23 @@
 'use strict';
 
+const channels = {
+  maxdome: (config) => {
+    return {
+      link: { package: 'http://www.maxdome.de', store: 'http://store.maxdome.de'}[config.area],
+      title: config.title,
+      ttl: 10
+    };
+  }
+};
+
 const proxies = {
   maxdome: (config) => {
     return {
       name: 'maxdome',
-      config: {
-        type: config.type,
-        area: config.area,
-        appid: '4a5fa26b',
-        apikey: 'f6cb16de3b67b8b9d64d047a00a9dc55',
-        clienttype: 'Webportal'
-      }
+      config: Object.assign(
+        { appid: '4a5fa26b', apikey: 'f6cb16de3b67b8b9d64d047a00a9dc55', clienttype: 'Webportal' },
+        config
+      )
     };
   }
 };
@@ -19,28 +26,28 @@ module.exports = [
   {
     rssfeed: {
       route: '/package-series',
-      channel: { title: 'Neue Serien im Paket', link: 'http://www.maxdome.de' }
+      channel: channels.maxdome({ area: 'package', title: 'Neue Serien im Paket' })
     },
     proxy: proxies.maxdome({ area: 'package', type: 'series' })
   },
   {
     rssfeed: {
       route: '/package-movies',
-      channel: { title: 'Neue Filme im Paket', link: 'http://www.maxdome.de' }
+      channel: channels.maxdome({ area: 'package', title: 'Neue Filme im Paket' })
     },
     proxy: proxies.maxdome({ area: 'package', type: 'movies' })
   },
   {
     rssfeed: {
       route: '/store-series',
-      channel: { title: 'Neue Serien im Store', link: 'http://store.maxdome.de' }
+      channel: channels.maxdome({ area: 'store', title: 'Neue Serien im Store' })
     },
     proxy: proxies.maxdome({ area: 'store', type: 'series' })
   },
   {
     rssfeed: {
       route: '/store-movies',
-      channel: { title: 'Neue Filme im Store', link: 'http://store.maxdome.de' }
+      channel: channels.maxdome({ area: 'store', title: 'Neue Filme im Store' })
     },
     proxy: proxies.maxdome({ area: 'store', type: 'movies' })
   }
