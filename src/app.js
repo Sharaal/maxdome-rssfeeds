a3080ca5583a0b049199433bca44a65ba3510352
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-28 10:47:25 +0200

    Move swig initialization back into the app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
+import co from 'co';
 import Koa from 'koa';
+import render from 'koa-swig';
 
 import Proxy from './proxy.js';
 import Router from './router.js';
 import rssfeeds from './rssfeeds.js';
-import swig from './swig.js';
 
 const app = new Koa();
-app.context.render = swig;
+app.context.render = co.wrap(render({ writeBody: false }));
 const router = Router({ rssfeeds, proxy: Proxy({ apikey: process.env.MAXDOME_APIKEY, appid: process.env.MAXDOME_APPID }) });
 app
   .use(router.routes())
--- src/swig.js
+++
@@ -1,4 +0,0 @@
-import co from 'co';
-import render from 'koa-swig';
-
-export default co.wrap(render({ writeBody: false }));
