6d8eaa1798679ffaec914e8eeefd0b1201e31a3c
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-04-29 13:00:43 +0200

    Use "request-promise" with transform option

--- src/proxy.js
+++ src/proxy.js
@@ -1,30 +1,24 @@
-import request from 'request';
+import rp from 'request-promise';
 
-export default ({ apikey, appid }) => async ({ area, type }) => {
-  return new Promise((resolve) => {
-    const options = {
-      url: 'https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?' +
-        [
-          'apikey=' + apikey,
-          'appid=' + appid,
-          'filter[]=new',
-          'filter[]=notUnlisted',
-          'filter[]=' + { package: 'hasPackageContent', store: 'availableWithoutPackage'}[area],
-          'filter[]=' + { movies: 'movies', seasons: 'seasons' }[type],
-          'sort[]=activeLicenseStart~desc'
-        ].join('&'),
-      headers: { accept: 'application/json', clienttype: 'Webportal', 'maxdome-origin': 'de' }
+export default ({ apikey, appid }) => async ({ area, type }) => rp.get({
+  url: 'https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?' +
+    [
+      'apikey=' + apikey,
+      'appid=' + appid,
+      'filter[]=new',
+      'filter[]=notUnlisted',
+      'filter[]=' + { package: 'hasPackageContent', store: 'availableWithoutPackage'}[area],
+      'filter[]=' + { movies: 'movies', seasons: 'seasons' }[type],
+      'sort[]=activeLicenseStart~desc'
+    ].join('&'),
+  headers: { accept: 'application/json', clienttype: 'Webportal', 'maxdome-origin': 'de' },
+  json: true,
+  transform: (data) => data.assetList.map((asset) => {
+    return {
+      guid: asset.id,
+      title: asset.title,
+      description: asset.descriptionShort,
+      link: { package: 'http://www.maxdome.de/', store: 'http://store.maxdome.de/'}[area] + asset.id
     };
-    request(options, (error, response, body) => {
-      const items = JSON.parse(body).assetList.map((asset) => {
-        return {
-          guid: asset.id,
-          title: asset.title,
-          description: asset.descriptionShort,
-          link: { package: 'http://www.maxdome.de/', store: 'http://store.maxdome.de/'}[area] + asset.id
-        };
-      });
-      resolve(items);
-    });
-  });
-};
+  })
+});
