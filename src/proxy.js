400ebe345000ef9c1afa988872e22d8dce5b307c
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-28 15:08:28 +0200

    Use the general heimdall proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,38 +1,25 @@
-import rp from 'request-promise';
+import AssetsQuery from './proxies/assets-query.js';
 
 export default class Proxy {
   constructor({ apikey, appid }) {
-    this.apikey = apikey;
-    this.appid = appid;
+    this.heimdall = require('./proxies/heimdall.js')({ apikey, appid });
   }
 
   async getAssets({ area, type }) {
-    const queryString = [
-      `apikey=${this.apikey}`,
-      `appid=${this.appid}`,
-      'filter[]=new',
-      'filter[]=notUnlisted',
-      `filter[]=${{ package: 'hasPackageContent', store: 'availableWithoutPackage' }[area]}`,
-      `filter[]=${{ movies: 'movies', seasons: 'seasons' }[type]}`,
-      'sort[]=activeLicenseStart~desc',
-    ].join('&');
-    const promise = rp.get({
-      url: `https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?${queryString}`,
-      headers: { accept: 'application/json', clienttype: 'Webportal', 'maxdome-origin': 'de' },
-      json: true,
-      transform: (data) => data.assetList.map((asset) => {
-        let title = asset.title;
-        if (asset['@class'] === 'MultiAssetTvSeriesSeason') {
-          title += ` (Season ${asset.number})`;
-        }
-        return {
-          title,
-          guid: asset.id,
-          description: asset.descriptionShort,
-          link: { package: 'http://www.maxdome.de/', store: 'http://store.maxdome.de/' }[area] + asset.id,
-        };
-      }),
+    const query = (new AssetsQuery())
+        .filter('new')
+        .filter('notUnlisted')
+        .filter({ package: 'hasPackageContent', store: 'availableWithoutPackage' }[area])
+        .filter({ movies: 'movies', seasons: 'seasons' }[type])
+        .sort('activeLicenseStart', 'desc');
+    const assets = await this.heimdall(query);
+    return assets.map(asset => {
+      return {
+        guid: asset.id,
+        title: asset.title,
+        description: asset.description,
+        link: { package: 'http://www.maxdome.de/', store: 'http://store.maxdome.de/' }[area] + asset.id
+      };
     });
-    return promise;
   }
 }
