fe6eae0af07fda88ae7ef316c4016f6112eca3ce
Christoph Herrmann <christoph.herrmann@maxdome.de>
2018-05-23 12:04:53 +0200

    Optional scheduling

--- src/schedule.js
+++ src/schedule.js
@@ -8,6 +8,11 @@ const maxdome = require('@dnode/request-maxdome').getRequestBuilder();
 (async () => {
   console.log('schedule started');
 
+  if (!process.env.FLASHBRIEFING_URL) {
+    console.log('schedule stopped because of missing "FLASHBRIEFING_URL"');
+    process.exit(0);
+  }
+
   const flashbriefings = await redis.getJSON('FLASHBRIEFINGS') || [];
   const latestFlashbriefing = flashbriefings[0];
 
